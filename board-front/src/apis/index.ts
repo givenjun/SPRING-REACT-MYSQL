fc0d916f18b67f7a119dc7620af9ff9c3a5be253
정준 <wjdwns0531@gmail.com>
2025-05-21 23:17:37 +0900

    2025-05-21 검색 페이지 API 연동

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -4,8 +4,8 @@ import { SignInResponseDto, SingUpResponseDto } from './response/auth';
 import { ResponseDto } from './response';
 import { GetSignInUserResponseDto } from './response/user';
 import { PatchBoardRequestDto, PostBoardRequestDto, PostCommentRequestDto } from './request/board';
-import { DeleteBoardResponseDto, GetBoardResponseDto, GetCommentListResponseDto, GetFavoriteListResponseDto, GetLatestBoardListResponseDto, GetTop3BoardListResponseDto, IncreaseViewCountResponseDto, PatchBoardResponseDto, PostBoardResponseDto, PostCommentResponseDto, PutFavoriteResponseDto } from './response/board';
-import { GetPopularListResponseDto } from './response/search';
+import { DeleteBoardResponseDto, GetBoardResponseDto, GetCommentListResponseDto, GetFavoriteListResponseDto, GetLatestBoardListResponseDto, GetSearchBoardListResponseDto, GetTop3BoardListResponseDto, IncreaseViewCountResponseDto, PatchBoardResponseDto, PostBoardResponseDto, PostCommentResponseDto, PutFavoriteResponseDto } from './response/board';
+import { GetPopularListResponseDto, GetRelationListResponseDto } from './response/search';
 
 const DOMAIN = 'http://localhost:4000';
 
@@ -49,6 +49,7 @@ export const signUpRequest = async (requestBody: SingUpRequestDto) => {
 const GET_BOARD_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}`;
 const GET_LATEST_BOARD_LIST_URL = () => `${API_DOMAIN}/board/latest-list`;
 const GET_TOP_3_BOARD_LIST_URL = () => `${API_DOMAIN}/board/top-3`;
+const GET_SEARCH_BOARD_LIST_URL = (searchWord: string, preSearchWord: string | null) => `${API_DOMAIN}/board/search-list/${searchWord}${preSearchWord ? '/' + preSearchWord : ''}`;
 const INCREASE_VIEW_COUNT_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/increase-view-count`;
 const GET_FAVORITE_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/favorite-list`;
 const GET_COMMENT_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/comment-list`;
@@ -90,6 +91,20 @@ export const getTop3BoardListRequest = async () => {
     const result = await axios.get(GET_TOP_3_BOARD_LIST_URL())
         .then(response => {
             const responseBody: GetTop3BoardListResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response.data) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        })
+    return result;
+}
+
+export const getSearchBoardListRequest = async (searchWord: string, preSearchWord: string | null) => {
+    const result = await axios.get(GET_SEARCH_BOARD_LIST_URL(searchWord, preSearchWord))
+        .then(response => {
+            const responseBody: GetSearchBoardListResponseDto = response.data;
             return responseBody;
         })
         .catch(error => {
@@ -213,11 +228,26 @@ export const deleteBoardRequest = async (boardNumber: number | string, accessTok
 }
 
 const GET_POPULAR_LIST_URL = () => `${API_DOMAIN}/search/popular-list`;
+const GET_RELATION_LIST_URL = (searchWord: string) => `${API_DOMAIN}/search/${searchWord}/relation-list`;
 
 export const getPopularListRequest = async () => {
     const result = await axios.get(GET_POPULAR_LIST_URL())
         .then(response => {
             const responseBody: GetPopularListResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response.data) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        });
+    return result;
+}
+
+export const GetRelationListRequest = async (searchWord: string) => {
+    const result = await axios.get(GET_RELATION_LIST_URL(searchWord))
+        .then(response => {
+            const responseBody: GetRelationListResponseDto = response.data;
             return responseBody;
         })
         .catch(error => {
--- board-front/src/views/Search/index.tsx
+++ board-front/src/views/Search/index.tsx
@@ -6,6 +6,11 @@ import { latestBoardListMock } from 'mocks';
 import BoardItem from 'components/BoardItem';
 import { SEARCH_PATH } from 'constant';
 import Pagination from 'components/Pagination';
+import { GetRelationListRequest, getSearchBoardListRequest } from 'apis';
+import { GetSearchBoardListResponseDto } from 'apis/response/board';
+import { ResponseDto } from 'apis/response';
+import { usePagination } from 'hooks';
+import { GetRelationListResponseDto } from 'apis/response/search';
 
 //          component: 검색 화면 컴포넌트          //
 export default function Search() {
@@ -15,23 +20,57 @@ export default function Search() {
 
   //          state: searchWord path variable 상태          //
   const { searchWord } = useParams();
+  //          state: 페이지네이션 관련 상태          //
+  const {
+          currentPage, currentSection, viewList, viewPageList, totalSection,
+          setCurrentPage, setCurrentSection, setTotalList
+      } = usePagination<BoardListItem>(5);
+  //          state: 이전 검색어 상태          //
+  const [preSearchWord, setPreSearchWord] = useState<string | null>(null);
   //          state: 검색 게시물 개수 상태          //
   const [count, setCount] = useState<number>(1);
   //          state: 검색 게시물 리스트 상태          //
-  const [searchBoardList, setSearchBoardList] = useState<BoardListItem[]>([]);
+  
   //          state: 연관 검색어 리스트 상태          //
-  const [relationList, setRelationList] = useState<string[]>([]);
+  const [relativeWordList, setRelativeWordListt] = useState<string[]>([]);
 
   //          event handler: 연관 검색어 클릭 이벤트 처리          //
-    const onRelationWordClickHandler = (word: string) => {
-      navigate(SEARCH_PATH(word));
-    }
+  const onRelationWordClickHandler = (word: string) => {
+    navigate(SEARCH_PATH(word));
+  }
 
-  //          effect: 첫 마운트 시 실행될 함수          //
+  //          function: get search board list response 처리 함수          //
+  const getSearchBoardListResponse = (responseBody: GetSearchBoardListResponseDto | ResponseDto | null) => {
+    if (!responseBody) return;
+    const { code } = responseBody;
+
+    if (code === 'DBE') alert('데이터베이스 오류입니다.');
+    if (code !== 'SU') return;
+
+    if (!searchWord) return;
+    const { searchList } = responseBody as GetSearchBoardListResponseDto;
+    setTotalList(searchList);
+    setCount(searchList.length);
+    setPreSearchWord(searchWord);
+  }
+  //          function: get relation list response 처리 함수          //
+  const GetRelationListResponse = (responseBody: GetRelationListResponseDto | ResponseDto | null) => {
+    if (!responseBody) return;
+    const { code } = responseBody;
+
+    if (code === 'DBE') alert('데이터베이스 오류입니다.');
+    if (code !== 'SU') return;
+
+    if (!searchWord) return;
+    const { relativeWordList } = responseBody as GetRelationListResponseDto;
+    setRelativeWordListt(relativeWordList);
+  }
+
+  //          effect: search word 상태 변경 시 실행될 함수          //
   useEffect(() => {
-    setSearchBoardList(latestBoardListMock);
-    // setRelationList(['안녕', '이제는 안녕', '이 말']);
-    setRelationList([]);
+    if (!searchWord) return;
+    getSearchBoardListRequest(searchWord, preSearchWord).then(getSearchBoardListResponse)
+    GetRelationListRequest(searchWord).then(GetRelationListResponse)
   }, [searchWord]);
 
   //          render: 검색 화면 컴포넌트 렌더링          //
@@ -46,15 +85,15 @@ export default function Search() {
         <div className='search-contents-box'>
           {count === 0 ? 
           <div className='search-contents-nothing'>{`검색 결과가 없습니다.`}</div> :
-          <div className='search-contents'>{searchBoardList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}</div>
+          <div className='search-contents'>{viewList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}</div>
           }
           <div className='search-relation-box'>
             <div className='search-relation-card'>
               <div className='search-relation-card-container'>
                 <div className='search-relation-card-title'>{'연관 검색어'}</div>
-                {relationList.length === 0 ?
+                {relativeWordList.length === 0 ?
                 <div className='search-relation-card-contents-nothing'>{`연관 검색어가 없습니다.`}</div> : 
-                <div className='search-relation-card-contents'>{relationList.map(word => <div className='word-badge' onClick={() => onRelationWordClickHandler(word)}>{word}</div>)}</div>
+                <div className='search-relation-card-contents'>{relativeWordList.map(word => <div className='word-badge' onClick={() => onRelationWordClickHandler(word)}>{word}</div>)}</div>
                 }
               </div>
             </div>
@@ -62,10 +101,14 @@ export default function Search() {
         </div>
         <div className='search-pagination-box'>
           {count !== 0 && 
-          <>페이지네이션{/* <Pagination /> */}</>
-  
-          }
-          
+          <Pagination
+            currentPage={currentPage}
+            currentSection={currentSection}
+            setCurrentPage={setCurrentPage}
+            setCurrentSection={setCurrentSection}
+            viewPageList={viewPageList}
+            totalSection={totalSection}
+          />}
         </div>
       </div>
     </div>
