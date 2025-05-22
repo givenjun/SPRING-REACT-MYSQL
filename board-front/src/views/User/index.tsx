4e34ac92497411a80ad6e4f5eaeba80e2a56c4f7
정준 <wjdwns0531@gmail.com>
2025-05-22 17:35:06 +0900

    2025-05-22 유저 페이지 하단 컴포넌트 구조 및 스타일 작성 및 이벤트 처리

--- board-front/src/views/User/index.tsx
+++ board-front/src/views/User/index.tsx
@@ -1,21 +1,31 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import './style.css'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import defaultProfileImage from 'assets/image/default-profile-image.png'
+import { BoardListItem } from 'types/interface';
+import { latestBoardListMock } from 'mocks';
+import BoardItem from 'components/BoardItem';
+import { BOARD_PATH, BOARD_WRITE_PATH, USER_PATH } from 'constant';
+import { useLoginUserStore } from 'stores';
 
 //          component: 유저 화면 컴포넌트          //
 export default function UserPage() {
 
   //          state: 마이페이지 여부 상태          //
+  const [isMyPage, setMyPage] = useState<boolean>(true);
+  //          state: userEmail path variable 상태          //
+  const { loginUser } = useLoginUserStore();
+  //          state: userEmail path variable 상태          //
   const { userEmail } = useParams();
+
+  //          function: 네비게이트 함수         //
+  const navigate = useNavigate();
 
   //          component: 유저 화면 상단 컴포넌트          //
   const UserTop = () => {
 
     //          state: 이미지 파일 인풋 참조 상태         //
     const imageInputRef = useRef<HTMLInputElement | null>(null);
-    //          state: 마이페이지 여부 상태          //
-    const [isMyPage, setMyPage] = useState<boolean>(true);
     //          state: 닉네임 변경 여부 상태          //
     const [isChangeNickname, setIsChangeNickname] = useState<boolean>(false);
     //          state: 닉네임 상태          //
@@ -102,9 +112,58 @@ export default function UserPage() {
   //          component: 유저 화면 하단 컴포넌트          //
   const UserBottom = () => {
 
+    //          state: 게시물 개수 상태          //
+    const [count, setCount] = useState<number>(1);
+    //          state: 게시물 리스트 상태          //
+    const [userBoardList, setUserBoardList] = useState<BoardListItem[]>([]);
+
+    //          event handler: 사이드 카드 클릭 이벤트 처리          //
+    const onSideCardClickHandler = () => {
+      if (isMyPage) navigate(BOARD_PATH() + '/' + BOARD_WRITE_PATH());
+      else if (loginUser) navigate(USER_PATH(loginUser.email));
+    }
+
+    //          effect: userEmail path variable 변경 시 실행할 함수         //
+    useEffect(() => {
+      setUserBoardList(latestBoardListMock)
+    }, [userEmail])
+
     //          render: 유저 화면 하단 컴포넌트 렌더링          //  
     return (
-      <div>유저 화면 하단</div>
+      <div id='user-bottom-wrapper'>
+        <div className='user-bottom-container'>
+          <div className='user-bottom-title'>{isMyPage ? '내 게시물 ' : '게시물 '}<span className='emphasis'>{count}</span></div>
+            <div className='user-bottom-contents-box'>
+              {count === 0 ?
+              <div className='user-bottom-contents-nothing'>{`게시물이 없습니다.`}</div> :
+              <div className='user-bottom-contents'>
+                {userBoardList.map(item => <BoardItem boardListItem={item}/>)}
+              </div>
+              }
+              <div className='user-bottom-side-box'>
+                <div className='user-bottom-side-card' onClick={onSideCardClickHandler}>
+                  <div className='user-bottom-side-container'>
+                    {isMyPage ? 
+                    <>
+                    <div className='icon-box'>
+                      <div className='icon edit-icon'></div>
+                    </div>
+                    <div className='user-bottom-side-text'>{`글쓰기`}</div>
+                    </> :
+                    <>
+                    <div className='user-bottom-side-text'>{`내 게시물로 가기`}</div>
+                    <div className='icon-box'>
+                      <div className='icon arrow-right-icon'></div>
+                    </div>
+                    </>
+                    }
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='user-bottom-pagination-box'></div>
+        </div>
+      </div>
     )
 
   }
